f50a99338d04f0a4081b527f9dc228a197ebeff9
Jeremy.Ngu <jeyncsgo@gmail.com>
2021-02-17 16:37:01 +0100

    Refactor items into subitems

--- src/components/Items/Project.vue
+++ src/components/Items/Project.vue
@@ -4,66 +4,34 @@
         <div class = "hrefProjectwrap" @click="openProjectLink()">
             <h3 class = "projectName">{{project.name}}</h3>
         </div>
-        <div class = "tagsContainer">
-            
-            <div v-bind:key="tag.id" v-for="tag in this.tags">
-                <p class = "tag" @click="openTagLink(tag.commit.web_url)">{{tag.name}}</p>
-            </div>
-        </div>
+        <Tags v-bind:project="this.project" />
     </div>
+
     <div class = "date">
         <img class = "calendarIcon" src="../../../public//CalendarIcon.png" alt="Calendar image" />
         <p class = "datep">Last activity : {{this.date.dateDDMMYY}} at {{this.date.dateTime}}</p>
     </div>
 
-    <div class="members">
-        <div v-bind:key="member.id" v-for="member in this.members">
-            <Member v-bind:member="member" />
-        </div>
-    </div>
-  
-    <div class = "pipeline">
-        <div class="hrefPipelinewrap" @click="openPipelineLink(pipelines[0]['web_url'])">
-            <div class = "successDiv" v-if="pipelines[0]['status'] === 'success'">
-                <p class = "successp"> Lastest commit passed </p>
-                <img class = "successIcon" src='../../../public/success-icon-10.png' alt = "success image"/>
-            </div>
-            <div v-else-if="pipelines[0]['status']==='failed'" class = "failedDiv" >
-                <p class = "failedp"> Lastest commit failed </p>
-                <img class = "failedIcon" src='../../../public/remove.png' alt = "success image"/>
-            </div>
-        </div>
-
-        <div v-if="typeof pipelines[0]['jobs_summary'] != 'undefined'" class = "numberTestPassed">
-            <p class = "numberTestPassedp"> {{pipelines[0]['jobs_summary']['success_count']}} / {{pipelines[0]['jobs_summary']['total_count']}} </p>
-
-            <div v-bind:key="job.id" v-for="job in pipelines[0]['jobs']">
-                <p @click="openJobLink(job.web_url)" v-if="job.status == 'failed'" class = "jobFailed"> {{job.name}} </p>
-                <p @click="openJobLink(job.web_url)" v-if="job.status == 'skipped'" class = "jobSkipped"> {{job.name}} </p>
-            </div>
-        </div>
-        
-        
-
-    </div>
-  
+    <Members v-bind:project="this.project" />
     
-
+    
+    <Pipeline v-bind:project="this.project"/>
   </div>
 </template>
 
-<script>
 
-//          <div v-if="typeof pipelines[0]['summary'] != 'undefined'" class = "numberTestPassed">
-//             <p class = "numberTestPassedp"> {{pipelines[0]['summary']['success_count']}} / {{pipelines[0]['summary']['total_count']}} </p>
-//         </div>
-    import Member from "./DisplayItem/Member"
+<script>
+    import Members from "./ProjectItems/Members"
+    import Pipeline from "./ProjectItems/Pipeline"
+    import Tags from "./ProjectItems/Tags"
     import axios from "axios";
 
     export default {
         name: "Project",
         components:{
-            Member,
+            Members,
+            Pipeline,
+            Tags,
         },
         props: ["project"],
 
@@ -72,12 +40,6 @@
                 displayMember: [],
                 members: [],
                 events: [],
-                // Initialized status a none so it can be read in the pipeline status fail or success html
-                pipelines: [
-                    {   status:"none",
-                        jobs:[],
-                    }
-                    ],
                 tags: [],
                 date: {},
             }
@@ -119,21 +81,6 @@
             console.error(error)
             })
 
-            // Loads tags of project
-            axios.get(this.project._links.self + "/repository/tags",{
-            headers: {
-                'Access-Control-Allow-Origin': 'GET',
-                'Content-Type': 'application/json',
-                "PRIVATE-TOKEN" : "SszFftmYGbwKHfoXWEzj"
-            }
-            })
-            .then((res) => {
-            this.tags = res.data
-            })
-            .catch((error) => {
-            console.error(error)
-            })
-
             // Load members of project
             axios.get(this.project._links.members,{
             headers: {
@@ -150,183 +97,11 @@
             .catch((error) => {
             console.error(error)
             })
-
-            // Load pipelines
-            axios.get(this.project._links.self + "/pipelines",{
-            headers: {
-                'Access-Control-Allow-Origin': 'GET',
-                'Content-Type': 'application/json',
-                "PRIVATE-TOKEN" : "SszFftmYGbwKHfoXWEzj"
-            }
-            })
-            .then((res) => {
-                if(res.data.length > 0){
-                    this.pipelines = res.data
-
-                    for(const pipelineIndex in res.data){
-
-                        // Load pipeline details
-                        axios.get(this.project._links.self + "/pipelines/" + this.pipelines[pipelineIndex]['id'],{
-                        headers: {
-                            'Access-Control-Allow-Origin': 'GET',
-                            'Content-Type': 'application/json',
-                            "PRIVATE-TOKEN" : "SszFftmYGbwKHfoXWEzj"
-                        }
-                        })
-                        .then((resDetail) => {
-                            this.$set(this.pipelines[pipelineIndex],"details",resDetail.data)
-                        })
-                        .catch((error) => {
-                        console.error(error)
-                        })
-                        
-                        // Load pipeline jobs
-                        
-                        axios.get(this.project._links.self + "/pipelines/" + this.pipelines[pipelineIndex]['id'] + "/jobs",{
-                        headers: {
-                            'Access-Control-Allow-Origin': 'GET',
-                            'Content-Type': 'application/json',
-                            "PRIVATE-TOKEN" : "SszFftmYGbwKHfoXWEzj"
-                        }
-                        })
-                        .then((resJob) => {
-                            if(this.project.id == 11){
-                                console.log(resJob)
-                                console.log(pipelineIndex)
-                                console.log(this.pipelines[pipelineIndex]['id'])
-                            }
-                            
-                            this.$set(this.pipelines[pipelineIndex],"jobs",resJob.data)
-
-                            var numberSuccess = 0
-                            var numberJobs = 0
-                            for(const i in resJob.data){
-                                if(resJob.data[i].status == "success"){
-                                    numberSuccess += 1
-                                }
-                                numberJobs += 1
-                            }
-
-                            this.$set(this.pipelines[pipelineIndex],"jobs_summary",{'success_count': numberSuccess, 'total_count': numberJobs})
-                        })
-                        .catch((error) => {
-                        console.error(error)
-                        })
-
-                    }
-                }
-            })
-            .catch((error) => {
-            console.error(error)
-            })
-
-            
-            
         },
     }
 </script>
 
 <style scoped>
-
-    .jobFailed{
-        margin-left: 1em;
-        background-color: rgba(233,19,19,0.8);
-        border-radius: 5px;
-        padding: 0.25em;
-        padding-left: 0.75em;
-        padding-right: 0.75em;
-        color: white;
-    }
-    .jobFailed:hover{
-        background-color: rgba(233,19,19,0.6);
-        cursor: pointer;
-    }
-
-    .jobSkipped{
-        margin-left: 1em;
-        background-color: rgba(105,105,105,0.84);
-        border-radius: 5px;
-        padding: 0.25em;
-        color: white;
-        padding-left: 0.75em;
-        padding-right: 0.75em;
-    }
-    .jobSkipped:hover{
-        background-color: rgba(105,105,105,0.54);
-        cursor: pointer;
-    }
-
-    .pipeline{
-        display: inline-flex;
-        width: 100%;
-    }
-
-    .hrefPipelinewrap{
-        margin-left:0.5em;
-        padding: 0.5em;
-    }
-    .hrefPipelinewrap:hover{
-        background-color: rgba(101,101,101,0.3);
-        border-radius: 5px;
-        cursor: pointer;
-        display: inline-block;
-        
-    }
-
-    .numberTestPassed{
-        text-align:center;
-        align-items: center;
-        margin-left: 1em;
-        display: inline-flex;
-    }
-
-    .successDiv{
-        display: inline-flex;   
-    }
-    .successp{
-        float: left;
-    }
-    .successIcon{
-        height: 20px;
-        float: left;
-        margin-left: 0.5em;
-    }
-
-    .failedDiv{
-        margin-left: 0.5em;
-        display: inline-flex;
-        text-align: center;
-    }
-
-    .failedIcon{
-        margin-left: 0.5em;
-        height: 20px;
-    }
-
-    .tagsContainer{
-        display: inline-table;
-        width: 40%;
-    }
-    .tag{
-        float: left;
-        margin: 0.5em;
-        padding: 0.25em;
-        font-size: 1em;
-        text-align: center;
-        background-color: rgba(202,202,202,0.64);
-        border-radius: 5px;
-        cursor: pointer;
-    }
-    .tag:hover{
-        float: left;
-        margin: 0.5em;
-        padding: 0.25em;
-        font-size: 1em;
-        text-align: center;
-        background-color: rgba(101,101,101,0.3);
-        border-radius: 5px;
-        cursor: pointer;
-    }
 
     .project{
         display: inline-block;
---
+++ src/components/Items/ProjectItems/Pipeline.vue
@@ -0,0 +1,196 @@
+<template>
+<div class = "pipeline">
+        <div class="hrefPipelinewrap" @click="openPipelineLink(pipelines[0]['web_url'])">
+            <div class = "successDiv" v-if="pipelines[0]['status'] === 'success'">
+                <p class = "successp"> Lastest commit passed </p>
+                <img class = "successIcon" src='../../../../public/success-icon-10.png' alt = "success image"/>
+            </div>
+            <div v-else-if="pipelines[0]['status']==='failed'" class = "failedDiv" >
+                <p class = "failedp"> Lastest commit failed </p>
+                <img class = "failedIcon" src='../../../../public/remove.png' alt = "success image"/>
+            </div>
+        </div>
+
+        <div v-if="typeof pipelines[0]['jobs_summary'] != 'undefined'" class = "numberTestPassed">
+            <p class = "numberTestPassedp"> {{pipelines[0]['jobs_summary']['success_count']}} / {{pipelines[0]['jobs_summary']['total_count']}} </p>
+            <div v-bind:key="job.id" v-for="job in pipelines[0]['jobs']">
+                <p @click="openJobLink(job.web_url)" v-if="job.status == 'failed'" class = "jobFailed"> {{job.name}} </p>
+                <p @click="openJobLink(job.web_url)" v-if="job.status == 'skipped'" class = "jobSkipped"> {{job.name}} </p>
+            </div>
+        </div>
+</div>
+</template>
+
+
+<script>
+    import axios from "axios";
+
+    export default {
+        name: "Pipeline",
+        components:{
+        },
+        props: ["project"],
+
+        data(){
+            return{
+                pipelines: [
+                    {   status:"none",
+                        jobs:[],
+                    }
+                ],
+            }
+        },
+        methods:{
+            openPipelineLink(link){
+                window.open(link,"_blank")
+            },
+            openJobLink(link){
+                window.open(link,"_blank")
+            }
+        },
+
+        created(){
+            // Load pipelines
+            axios.get(this.project._links.self + "/pipelines",{
+            headers: {
+                'Access-Control-Allow-Origin': 'GET',
+                'Content-Type': 'application/json',
+                "PRIVATE-TOKEN" : "SszFftmYGbwKHfoXWEzj"
+            }
+            })
+            .then((res) => {
+                if(res.data.length > 0){
+                    this.pipelines = res.data
+
+                    for(const pipelineIndex in res.data){
+
+                        // Load pipeline details
+                        axios.get(this.project._links.self + "/pipelines/" + this.pipelines[pipelineIndex]['id'],{
+                        headers: {
+                            'Access-Control-Allow-Origin': 'GET',
+                            'Content-Type': 'application/json',
+                            "PRIVATE-TOKEN" : "SszFftmYGbwKHfoXWEzj"
+                        }
+                        })
+                        .then((resDetail) => {
+                            this.$set(this.pipelines[pipelineIndex],"details",resDetail.data)
+                        })
+                        .catch((error) => {
+                        console.error(error)
+                        })
+                        
+                        // Load pipeline jobs
+                        
+                        axios.get(this.project._links.self + "/pipelines/" + this.pipelines[pipelineIndex]['id'] + "/jobs",{
+                        headers: {
+                            'Access-Control-Allow-Origin': 'GET',
+                            'Content-Type': 'application/json',
+                            "PRIVATE-TOKEN" : "SszFftmYGbwKHfoXWEzj"
+                        }
+                        })
+                        .then((resJob) => {
+                            this.$set(this.pipelines[pipelineIndex],"jobs",resJob.data)
+
+                            var numberSuccess = 0
+                            var numberJobs = 0
+                            for(const i in resJob.data){
+                                if(resJob.data[i].status == "success"){
+                                    numberSuccess += 1
+                                }
+                                numberJobs += 1
+                            }
+
+                            this.$set(this.pipelines[pipelineIndex],"jobs_summary",{'success_count': numberSuccess, 'total_count': numberJobs})
+                        })
+                        .catch((error) => {
+                        console.error(error)
+                        })
+
+                    }
+                }
+            })
+            .catch((error) => {
+            console.error(error)
+            })
+
+            
+            
+        },
+    }
+</script>
+
+<style scoped>
+.numberTestPassed{
+        text-align:center;
+        align-items: center;
+        margin-left: 1em;
+        display: inline-flex;
+    }
+
+.successDiv{
+    display: inline-flex;   
+}
+.successp{
+    float: left;
+}
+.successIcon{
+    height: 20px;
+    float: left;
+    margin-left: 0.5em;
+}
+
+.failedDiv{
+    margin-left: 0.5em;
+    display: inline-flex;
+    text-align: center;
+}
+
+.failedIcon{
+    margin-left: 0.5em;
+    height: 20px;
+}
+
+.pipeline{
+        display: inline-flex;
+        width: 100%;
+    }
+
+.hrefPipelinewrap{
+    margin-left:0.5em;
+    padding: 0.5em;
+}
+.hrefPipelinewrap:hover{
+    background-color: rgba(101,101,101,0.3);
+    border-radius: 5px;
+    cursor: pointer;
+    display: inline-block;
+    
+}
+.jobFailed{
+        margin-left: 1em;
+        background-color: rgba(233,19,19,0.8);
+        border-radius: 5px;
+        padding: 0.25em;
+        padding-left: 0.75em;
+        padding-right: 0.75em;
+        color: white;
+    }
+.jobFailed:hover{
+    background-color: rgba(233,19,19,0.6);
+    cursor: pointer;
+}
+
+.jobSkipped{
+    margin-left: 1em;
+    background-color: rgba(105,105,105,0.84);
+    border-radius: 5px;
+    padding: 0.25em;
+    color: white;
+    padding-left: 0.75em;
+    padding-right: 0.75em;
+}
+.jobSkipped:hover{
+    background-color: rgba(105,105,105,0.54);
+    cursor: pointer;
+}
+</style>
