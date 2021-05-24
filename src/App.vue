e1e5ffccf87de4ffc7ebdd3e2e607b8700c551f6
Jeremy.Ngu <jeyncsgo@gmail.com>
2021-05-24 16:09:54 +0200

    fixed reset

--- src/App.vue
+++ src/App.vue
@@ -283,11 +283,19 @@ export default {
     },
 
     resetsearch() {
-      if(this.projectsQuery.length != this.projects.length) {
+      if(!this.projectsQuery){
         this.filterTitle = [];
         this.reset = this.reset + 1;
         this.projectsQuery = this.projects;
       }
+      else{
+        if(this.projectsQuery.length != this.projects.length) {
+          this.filterTitle = [];
+          this.reset = this.reset + 1;
+          this.projectsQuery = this.projects;
+        }
+      }
+      
       
       
     },
